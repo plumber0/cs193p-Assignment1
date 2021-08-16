53b9914cefd5193ec5e48d732f50f3398cf28330
Seugwoo Yoo <seungwoo@deepnatural.ai>
2021-08-16 16:43:58 +0900

    Initial Commit

---
+++ cs193p-Assignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs193p-Assignment1
+//
+//  Created by SeungWoo on 2021/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs193p-Assignment1/cs193p_Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_Assignment1App.swift
+//  cs193p-Assignment1
+//
+//  Created by SeungWoo on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
